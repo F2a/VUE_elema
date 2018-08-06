86556cd1876bbc996106677883163de5704e36e9
FangRui <arnold3000@163.com>
2018-08-06 15:36:57 +0800

    8.6 - todo finished

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -2,6 +2,18 @@
   <h1>I'm header</h1>
 </template>
 <script>
+  export default {
+    name: 'Header',
+    data () {
+      return {
+        txt: '',
+        todo: [],
+      }
+    },
+    mounted() {
+
+    },
+  }
 </script>
 <style>
 </style>
