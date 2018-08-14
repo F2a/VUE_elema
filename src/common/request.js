158bb6a800cc9cbf65b2b239d16eaa30354f27bb
FangRui <arnold3000@163.com>
2018-08-14 11:50:58 +0800

    8.14 - axios封装

--- src/common/request.js
+++ src/common/request.js
@@ -1,14 +1,41 @@
-/**
- * Created by Administrator on 2018/8/6.
- */
 import axios from 'axios'
-let axiosIns  = axios.create({
+
+
+const axiosIns  = axios.create({
   baseURL: 'http://www.phonegap100.com/',
   timeout: 5000,
   headers: {
     'Content-Type': 'application/json',
   },
 });
+// 添加请求拦截器
+axiosIns.interceptors.request.use(config => {
+  // 在发送请求之前做些什么
+  return config;
+}, error => {
+  // 对请求错误做些什么
+  return Promise.reject(error);
+});
+
+// 添加响应拦截器
+axiosIns.interceptors.response.use(response => {
+  // 对响应数据做点什么
+  const statusFlag = statusError(response.data.status);
+
+  return response;
+}, error => {
+  // 对响应错误做点什么
+  err(error);
+  return Promise.reject(error);
+});
+function statusError(status) { //响应后台的状态错误
+  let errorStatus = false
+  switch (status) {
+    case 404:
+      errorStatus = 'tokenInvalid';
+      break
+  }
+}
 function err(info) {
   if (info === 'TypeError: Failed to fetch') {
     const result = { errmsg: '网络未连接，请检查网络设置' };
@@ -16,23 +43,6 @@ function err(info) {
     return result;
   }
 }
-function parseJSON(response) {
-  console.log(response);
-  try {
-    if (response.statusText === 'OK') {
 
-    }
-  } catch (error) {
 
-  }
-  return response;
-}
-export default function request(url, option){
-  axiosIns({
-    url,
-    ...option
-  })
-  .then(parseJSON)
-  .catch(err);
-}
-
+export default axiosIns
